2f0bffc9b7ed9d2a13d911cc65ccae7671482c29
Muhammad Al-Halaby <moj1323@gmail.com>
2021-12-25 01:26:00 +0200

    Add fourier transform and its inverse along with the needed function to calculate the magnitude

---
+++ backend/fourier.py
@@ -0,0 +1,50 @@
+import numpy as np
+import cv2 as cv
+
+
+def shifted_dft(img):
+    """
+    Transform image from spatial domain to frequency domain by preforming forward Discrete Fourier transform and then
+    shift the zero-frequency component to the center of the spectrum.
+
+    Inputs:
+        - img: A numpy array of any shape containing the image.
+
+    Returns:
+        - shifted_dft: a numpy array containing the transformed image with the same size but with 2 channels
+        (real and complex).
+    """
+    img_dft = cv.dft(np.float32(img), flags=cv.DFT_COMPLEX_OUTPUT)
+    shifted_dft = np.fft.fftshift(img_dft)
+    return shifted_dft
+
+
+def dft_magnitude(shifted_dft):
+    """
+    Compute the magnitude spectrum of an image in the frequency domain for analysis and visualization purposes.
+
+    Inputs:
+        - shifted_dft: A numpy array of shape (H, W, 2) containing the image in frequency domain with the
+        zero-frequency component shifted to the center of the spectrum.
+
+    Returns:
+        - magnitude_spectrum: a numpy array of shape (H, W) containing intensity values.
+    """
+    magnitude_spectrum = 20 * np.log(cv.magnitude(shifted_dft[:, :, 0], shifted_dft[:, :, 1]))
+    return magnitude_spectrum
+
+
+def inverse_shifted_dft(shifted_dft):
+    """
+    Calculates the inverse Discrete Fourier Transform of an image after shifting the zero frequency component (DC component) to be at top left corner.
+
+    Inputs:
+        - shifted_dft: A numpy array of shape (H, W, 2) containing the image in frequency domain with the
+        zero-frequency component shifted to the center of the spectrum.
+
+    Returns:
+        - img: a numpy array of shape (H, W) containing the original image.
+    """
+    img_dft = np.fft.ifftshift(shifted_dft)
+    img = cv.idft(img_dft, flags=cv.DFT_REAL_OUTPUT)
+    return img
