f1edb87dda6f84473cca34cb9cd231a5920e51cf
Muhammad Al-Halaby <moj1323@gmail.com>
2021-12-25 01:42:07 +0200

    update test file

--- test.py
+++ test.py
@@ -33,28 +33,37 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 '''
 
-cv2.imshow("orig", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow("orig", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
+# blurredImg = gaussianFilter(img, 15)
+# cv2.imshow("blurred", blurredImg)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
+# soble_edge_img = sobel_filter(img)
+# cv2.imshow("soble_edge_img", soble_edge_img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
+# laplace_edge_img = laplacian_filter(img, ksize=7)
+# cv2.imshow("laplace_edge_img", laplace_edge_img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
+#
+#
+# img_with_periodic_noise = add_periodic_noise(img_gray)
+# cv2.imshow("img_with_periodic_noise", img_with_periodic_noise)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-blurredImg = gaussianFilter(img, 15)
-cv2.imshow("blurred", blurredImg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-soble_edge_img = sobel_filter(img)
-cv2.imshow("soble_edge_img", soble_edge_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-laplace_edge_img = laplacian_filter(img, ksize=7)
-cv2.imshow("laplace_edge_img", laplace_edge_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-
-img_with_periodic_noise = add_periodic_noise(img_gray)
-cv2.imshow("img_with_periodic_noise", img_with_periodic_noise)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+salt_and_pepper_img = add_salt_and_pepper_noise(img, 0.30, 0.8)
+plt.subplot(1, 3, 1)
+plt.imshow(salt_and_pepper_img)
+plt.subplot(1, 3, 2)
+plt.imshow(median_filter(salt_and_pepper_img,  5))
+plt.subplot(1, 3, 3)
+plt.imshow(averaging_filter(salt_and_pepper_img,  5, 5))
+plt.show()
